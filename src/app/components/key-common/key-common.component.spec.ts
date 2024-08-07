c0e877e0dc0415441bd2a685ae162cc5c9d0f169
Joseph Kelley <josephkelley91@gmail.com>
2024-08-07 09:23:07 -0400

    Adds initial key-common component to project.

---
+++ src/app/components/key-common/key-common.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { KeyCommonComponent } from './key-common.component';
+
+describe('KeyCommonComponent', () => {
+  let component: KeyCommonComponent;
+  let fixture: ComponentFixture<KeyCommonComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [KeyCommonComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(KeyCommonComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/components/key-common/key-common.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-key-common',
+  standalone: true,
+  imports: [],
+  templateUrl: './key-common.component.html',
+  styleUrl: './key-common.component.scss'
+})
+export class KeyCommonComponent {
+
+}
