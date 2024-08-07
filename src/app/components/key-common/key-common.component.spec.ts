d702fa5c60a4b02d7e32723bffaa203ea7695670
Joseph Kelley <josephkelley91@gmail.com>
2024-08-07 09:33:44 -0400

    Clean up.

--- src/app/components/key-common/key-common.component.spec.ts
+++ src/app/components/key-common/key-common.component.spec.ts
@@ -3,21 +3,21 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { KeyCommonComponent } from './key-common.component';
 
 describe('KeyCommonComponent', () => {
-  let component: KeyCommonComponent;
-  let fixture: ComponentFixture<KeyCommonComponent>;
+    let component: KeyCommonComponent;
+    let fixture: ComponentFixture<KeyCommonComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [KeyCommonComponent]
-    })
-    .compileComponents();
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            imports: [KeyCommonComponent]
+        })
+            .compileComponents();
 
-    fixture = TestBed.createComponent(KeyCommonComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+        fixture = TestBed.createComponent(KeyCommonComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
