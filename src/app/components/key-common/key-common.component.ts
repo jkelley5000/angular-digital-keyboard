ae075ea02c4529b8ad90027484139d7085b65417
Joseph Kelley <josephkelley91@gmail.com>
2024-08-08 10:53:11 -0400

    Configured key-common component to properly accept keys data, and render keys data according to design specifications.

--- src/app/components/key-common/key-common.component.ts
+++ src/app/components/key-common/key-common.component.ts
@@ -10,10 +10,31 @@ import { Key } from '../../models/key';
     styleUrl: './key-common.component.scss'
 })
 export class KeyCommonComponent {
-    @Input() keyObject: Key = {
-        row: 0,
-        levels: 0,
-        value: [],
-        type: 0
-    };
+    @Input() keys: Array<Key> = [];
+
+    keysRowOne: Array<Key> = [];
+    keysRowTwo: Array<Key> = [];
+    keysRowThree: Array<Key> = [];
+    keysRowFour: Array<Key> = [];
+    keysRowFive: Array<Key> = [];
+
+    ngOnInit(): void {
+        this.keys.map(key => {
+            if (key.row === 1) {
+                this.keysRowOne.push(key);
+            }
+            if (key.row === 2) {
+                this.keysRowTwo.push(key);
+            }
+            if (key.row === 3) {
+                this.keysRowThree.push(key);
+            }
+            if (key.row === 4) {
+                this.keysRowFour.push(key);
+            }
+            if (key.row === 5) {
+                this.keysRowFive.push(key);
+            }
+        });
+    }
 }
