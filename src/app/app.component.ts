56541f086519fc0890ef5cbe96e695cff2c82ce3
Joseph Kelley <josephkelley91@gmail.com>
2024-08-07 20:50:58 -0400

    Adds initial design styles to key-common component. Adds key-common component to app component for initial development.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { KeyCommonComponent } from './components/key-common/key-common.component';
+import { Key } from './models/key';
+import { KeySpecialComponent } from './components/key-special/key-special.component';
 
 @Component({
     selector: 'app-root',
     standalone: true,
-    imports: [RouterOutlet],
+    imports: [RouterOutlet, KeyCommonComponent, KeySpecialComponent],
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss'
 })
 export class AppComponent {
+    demoKey0: Key = { "row": 1, "levels": 2, "value": ["~", "`"], "type": 0 };
+    demoKey1: Key = { "row": 2, "levels": 1, "value": ["Tab"], "type": 1 };
+    demoKey2: Key = { "row": 1, "levels": 1, "value": ["Backspace"], "type": 2 };
+    demoKey3: Key = { "row": 4, "levels": 1, "value": ["Shift"], "type": 3 };
+    demoKey4: Key = { "row": 5, "levels": 1, "value": [""], "type": 4 };
 
+    demoKey5: Key = { "row": 5, "levels": 1, "value": [""], "type": 5 };
 }
--- src/app/components/key-common/key-common.component.ts
+++ src/app/components/key-common/key-common.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Key } from '../../models/key';
 
 @Component({
-  selector: 'app-key-common',
-  standalone: true,
-  imports: [],
-  templateUrl: './key-common.component.html',
-  styleUrl: './key-common.component.scss'
+    selector: 'app-key-common',
+    standalone: true,
+    imports: [CommonModule],
+    templateUrl: './key-common.component.html',
+    styleUrl: './key-common.component.scss'
 })
 export class KeyCommonComponent {
-
+    @Input() keyObject: Key = {
+        row: 0,
+        levels: 0,
+        value: [],
+        type: 0
+    };
 }
